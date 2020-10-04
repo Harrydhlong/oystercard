9c5acfa772f2156213887747a9031b2e4edb04fd
Harry Long <harrydhlong@gmail.com>
2020-10-04 15:53:24 +0100

    Added new functionality - records name of the station you tapped in at

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,14 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:station) { double :station}
+
+  it 'stores the entry station' do
+    subject.top_up(2)
+    subject.touch_in(station)
+    expect(subject.entry_station).to eq station
+  end
+  
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
@@ -23,28 +31,25 @@ describe Oystercard do
   end
 
   it 'can touch in' do
-    if subject.balance > Oystercard::MINIMUM_BALANCE
-      subject.touch_in
-      expect(subject).to be_in_journey
-    end
+    subject.top_up(2)
+    subject.touch_in(station)
+    expect(subject).to be_in_journey
   end
 
   it 'can touch out' do
-    if subject.balance > Oystercard::MINIMUM_BALANCE
-      subject.touch_in
-      subject.touch_out
-      expect(subject).not_to be_in_journey
-    end
+    subject.top_up(2)
+    subject.touch_in(station)
+    subject.touch_out
+    expect(subject).not_to be_in_journey
   end
 
   it 'will not touch in if below minimum balance' do
-    expect { subject.touch_in }.to raise_error "Insufficient balance to touch in"
+    expect { subject.touch_in(station) }.to raise_error "Insufficient balance to touch in"
   end
 
   it 'deducts balance when you touch_out' do
-    if subject.balance > Oystercard::MINIMUM_BALANCE
-      subject.touch_in
-      expect { subject.touch_out }.to change { subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
-    end
+    subject.top_up(2)
+    subject.touch_in(station)
+    expect { subject.touch_out }.to change { subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
   end
 end
